503596c6cf547433e8c4c9f7884dea0e8405e770
Enesakarr50 <enesakar205@gmail.com>
2024-07-22 15:03:43 +0300

    cumacumq

--- Assets/Scripts/ActiveSkill.cs
+++ Assets/Scripts/ActiveSkill.cs
@@ -6,33 +6,57 @@ public class ActiveSkill : ScriptableObject
     public Sprite icon;
     public float cooldown;
 
-
     // Aktif yetenek için bir metot tanýmlayýn (örnek olarak)
     public virtual void Activate(GameObject character)
     {
-       
+        // Yetenek aktif edildiðinde yapýlacak iþlemler
     }
-
 }
+
 [CreateAssetMenu(fileName = "Bazuka", menuName = "ActiveSkillTypes/Bazuka")]
 public class Bazuka : ActiveSkill
 {
-    public GameObject fireballPrefab; // Alev topu prefabý
+    public GameObject fireballPrefab; 
     public int speed;
-    public int knocBackPower;
+    public int knockBackPower;
     public int radius;
+
     public override void Activate(GameObject character)
     {
-        GameObject proj = Instantiate(fireballPrefab);  
+        
+      
     }
 }
-[CreateAssetMenu(fileName = "Inviciblety", menuName = "ActiveSkillTypes/Inviciblety")]
 
+[CreateAssetMenu(fileName = "Inviciblety", menuName = "ActiveSkillTypes/Inviciblety")]
 public class Inviciblety : ActiveSkill
 {
-    public int duration;
+    public float duration; 
+
     public override void Activate(GameObject character)
     {
-       character.GetComponent<SpriteRenderer>().enabled = false;          
+        
+    }
+}
+
+[CreateAssetMenu(fileName = "MagicWall", menuName = "ActiveSkillTypes/MagicWall")]
+public class MagicWall : ActiveSkill
+{
+    public float duration;
+
+    public override void Activate(GameObject character)
+    {
+
+    }
+}
+
+[CreateAssetMenu(fileName = "Catapult", menuName = "ActiveSkillTypes/Catapult")]
+public class Catapult : ActiveSkill
+{
+    public float duration;
+
+    public override void Activate(GameObject character)
+    {
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -17,8 +16,7 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
     public bool isGrounded;
     private bool isDashing;
-   
-    
+    private bool isSkillOnCooldown;
 
     private void Start()
     {
@@ -29,13 +27,12 @@ public class Player : MonoBehaviour
         maxJumpCount = Character.characterJumpCount;
         rb2d = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         // Karakterin sola ve saða hareket etmesi
         float moveInput = Input.GetAxis("Horizontal");
-    
-         rb2d.velocity = new Vector2(moveInput * movementSpeed, rb2d.velocity.y);
-        
+        rb2d.velocity = new Vector2(moveInput * movementSpeed, rb2d.velocity.y);
 
         // Karakterin zýplamasý ve double jump
         if (Input.GetKeyDown(KeyCode.W) && jumpCount < maxJumpCount)
@@ -43,13 +40,42 @@ public class Player : MonoBehaviour
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
             jumpCount++;
         }
-        if (Input.GetKeyDown(KeyCode.E) && Character._activeSkill != null)
+
+        if (Input.GetKeyDown(KeyCode.E) && Character._activeSkill != null && !isSkillOnCooldown)
         {
-            Character._activeSkill.Activate(gameObject);
+           // Character._activeSkill.Activate(gameObject);
+            
+
+            switch (Character._activeSkill)
+            {
+                case Inviciblety:
+                    Debug.Log("a");
+                    StartCoroutine(TempInvincibility((Inviciblety)Character._activeSkill));
+                    break;
+
+                case Bazuka:
+                    Debug.Log("a");
+                    StartCoroutine(bazukaSpawn((Bazuka)Character._activeSkill));
+                    break;
+
+                case MagicWall :
+                    Debug.Log("c");
+                    StartCoroutine(magicwall((MagicWall)Character._activeSkill));
+                    break;
+
+                case Catapult:
+                    Debug.Log("d");
+                    StartCoroutine(catapult((Catapult)Character._activeSkill));
+                    break;
+
+
+
+
+            }
         }
 
         // Dash
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing) // Varsayýlan Fire3 tuþu sol Shift'tir
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
         {
             if (moveInput > 0)
             {
@@ -60,9 +86,8 @@ public class Player : MonoBehaviour
                 transform.position += new Vector3(-2, 0, 0);
             }
             isDashing = true;
+            StartCoroutine(DashCount());
         }
-
-        
 
         // Karakterin yönünü deðiþtirme (sola ve saða bakma)
         if (moveInput > 0)
@@ -81,11 +106,45 @@ public class Player : MonoBehaviour
             jumpCount = 0; // Zýplama sayacýný sýfýrlayýn
         }
     }
-    public IEnumerator dashCount()
+
+    private IEnumerator TempInvincibility(Inviciblety skill)
+    {
+        isSkillOnCooldown = true;
+        PlayerSprite.enabled = false;
+        yield return new WaitForSeconds(skill.duration);
+        PlayerSprite.enabled = true;
+        yield return new WaitForSeconds(skill.cooldown);
+        isSkillOnCooldown = false;
+    }
+
+    private IEnumerator bazukaSpawn(Bazuka Skill)
+    {
+        isSkillOnCooldown = true;
+        GameObject bazukaa = Instantiate(Skill.fireballPrefab);
+        Debug.Log("spawn");
+        yield return new WaitForSeconds(Skill.cooldown);
+        isSkillOnCooldown = false;
+    }
+
+    private IEnumerator magicwall(MagicWall Skill)
+    {
+        isSkillOnCooldown = true;
+        Debug.Log("magic okeyss");
+        yield return new WaitForSeconds(Skill.cooldown);
+        isSkillOnCooldown = false;
+    }
+
+    private IEnumerator catapult(Catapult Skill)
+    {
+        isSkillOnCooldown = true;
+        Debug.Log("cata okeyss");
+        yield return new WaitForSeconds(Skill.cooldown);
+        isSkillOnCooldown = false;
+    }
+
+    private IEnumerator DashCount()
     {
         yield return new WaitForSeconds(2);
         isDashing = false;
     }
 }
-
-
