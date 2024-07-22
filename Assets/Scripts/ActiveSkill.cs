b8d59c0360aedc7e9ca67f6f534ad749f5d5eb9b
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-22 12:19:24 +0300

    nnm
    nvm

--- Assets/Scripts/ActiveSkill.cs
+++ Assets/Scripts/ActiveSkill.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ActiveSkill : ScriptableObject
@@ -5,14 +6,16 @@ public class ActiveSkill : ScriptableObject
     public Sprite icon;
     public float cooldown;
 
+
     // Aktif yetenek için bir metot tanýmlayýn (örnek olarak)
     public virtual void Activate(GameObject character)
     {
-        // Yetenek aktivasyon kodu burada olacak
+       
     }
+
 }
-[CreateAssetMenu(fileName = "New", menuName = "ActiveSkillTypes/ThrowableSkills")]
-public class ThrowableSkill : ActiveSkill
+[CreateAssetMenu(fileName = "Bazuka", menuName = "ActiveSkillTypes/Bazuka")]
+public class Bazuka : ActiveSkill
 {
     public GameObject fireballPrefab; // Alev topu prefabý
     public int speed;
@@ -20,16 +23,16 @@ public class ThrowableSkill : ActiveSkill
     public int radius;
     public override void Activate(GameObject character)
     {
-        GameObject proj = Instantiate(fireballPrefab);
+        GameObject proj = Instantiate(fireballPrefab);  
     }
 }
-[CreateAssetMenu(fileName = "New", menuName = "ActiveSkillTypes/Usable")]
+[CreateAssetMenu(fileName = "Inviciblety", menuName = "ActiveSkillTypes/Inviciblety")]
 
-public class UsableSkill : ActiveSkill
+public class Inviciblety : ActiveSkill
 {
     public int duration;
     public override void Activate(GameObject character)
     {
-        character.transform.position = new Vector3(0, 0, 0);
+            character.GetComponent<SpriteRenderer>().enabled = false;          
     }
 }
