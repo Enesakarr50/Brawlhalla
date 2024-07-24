385f4789cabf3a91a880a0fe1fec03c06693b794
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-24 14:45:10 +0300

    yok

--- Assets/Scripts/DynamicCamera.cs
+++ Assets/Scripts/DynamicCamera.cs
@@ -1,49 +1,63 @@
-using UnityEditor.U2D.Animation;
 using UnityEngine;
-
+using Cinemachine;
 
 public class DynamicCamera : MonoBehaviour
 {
-    public Transform player1;
-    public Transform player2;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public CinemachineVirtualCamera virtualCamera;
     public float minZoom = 40f;
     public float maxZoom = 10f;
     public float zoomLimiter = 50f;
-    public Camera mainCamera;
 
-   
-    private void LateUpdate()
+    public bool IsTwo = false;
+
+    private CinemachineTargetGroup targetGroup;
+
+    private void Start()
     {
-        if (player1 == null || player2 == null) return;
-
-        MoveCamera();
-        ZoomCamera();
+        targetGroup = GetComponent<CinemachineTargetGroup>();
     }
 
     private void Update()
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        if(players == null || players.Length == 2)
+        if (players.Length == 2 && IsTwo == false)
         {
-            player1 = players[0].transform;
-            player2 = players[1].transform;
-        } 
-        
+            targetGroup.m_Targets[0].target = players[0].transform;
+            targetGroup.m_Targets[1].target = players[1].transform;
+            IsTwo = true;
+        }
+  
     }
-    void MoveCamera()
+    private void LateUpdate()
     {
-        Vector3 middlePoint = (player1.position + player2.position) / 2;
-        Vector3 desiredPosition = middlePoint + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        if (targetGroup == null || virtualCamera == null) return;
+
+        AdjustZoom();
     }
 
-    void ZoomCamera()
+    void AdjustZoom()
     {
-        float distance = Vector3.Distance(player1.position, player2.position);
-        float newZoom = Mathf.Lerp(maxZoom, minZoom, distance / zoomLimiter);
-        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, newZoom, Time.deltaTime);
+        if (IsTwo)
+        {
+            float distance = GetGreatestDistance();
+            float newZoom = Mathf.Lerp(maxZoom, minZoom, distance / zoomLimiter);
+            virtualCamera.m_Lens.OrthographicSize = newZoom;
+        }
+        
+
+    }
+
+    float GetGreatestDistance()
+    {
+        if (targetGroup.m_Targets.Length < 2) return 0f;
+
+        Bounds bounds = new Bounds(targetGroup.m_Targets[0].target.position, Vector3.zero);
+
+        for (int i = 1; i < targetGroup.m_Targets.Length; i++)
+        {
+            bounds.Encapsulate(targetGroup.m_Targets[i].target.position);
+        }
+
+        return bounds.size.x;
     }
 }
