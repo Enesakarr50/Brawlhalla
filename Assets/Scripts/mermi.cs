5964ac9b6216667b483229361d6cc5a04ad10465
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-24 16:15:32 +0300

    ol lütfen ol

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -1,22 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager;
-using UnityEditor.U2D.Animation;
+using Photon.Pun;
 using UnityEngine;
 
-public class mermi : MonoBehaviour
+public class mermi : MonoBehaviourPun
 {
     public CharacterData _cD;
-
+    public Rigidbody2D _rigidbody2;
    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-        if (rb != null)
+        _rigidbody2 = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (_rigidbody2 != null)
         {
-            Vector2 pushDirection = rb.position - (Vector2)transform.position;
-            rb.AddForce(pushDirection.normalized * _cD.KnockBackRate, ForceMode2D.Impulse);
-            Destroy(gameObject);
+            photonView.RPC("isKock", RpcTarget.AllBuffered, true);
         }
+    }
+
+    [PunRPC]
+    void KnockBack(bool isKock)
+    {
+        Vector2 pushDirection = _rigidbody2.position - (Vector2)transform.position;
+        _rigidbody2.AddForce(pushDirection.normalized * _cD.KnockBackRate, ForceMode2D.Impulse);
+        Destroy(gameObject);
     }
 }
