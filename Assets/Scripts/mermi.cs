c683370e61841715874a1c5707a4f3fdef33db82
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 16:42:49 +0300

    31
    31

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -1,1 +1,11 @@
+using Photon.Pun;
+using UnityEngine;
 
+public class Mermi : MonoBehaviourPun
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PhotonNetwork.Destroy(gameObject);
+    }
+}
