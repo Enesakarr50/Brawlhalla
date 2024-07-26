93643db7478a98279dec9ab2f172cfbac024b51c
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 16:04:09 +0300

    gg
    gg

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -3,41 +3,30 @@ using UnityEngine;
 
 public class mermi : MonoBehaviourPun
 {
-    private Rigidbody2D _rigidbody2D;
-    private Vector2 _direction; // Merminin hareket yönü
     public CharacterData _cD;
-
-    private void Start()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _direction = _rigidbody2D.velocity.normalized; // Baþlangýçta merminin yönünü belirleyin
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             PhotonView pv = collision.gameObject.GetComponent<PhotonView>();
-            if (pv != null && !pv.IsMine) // Çarpýlan oyuncu yerel deðilse
+            if (pv != null && !pv.IsMine)
             {
-                // Merminin hareket yönünü gönder
-                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, _direction);
+                Vector3 pushDirection = collision.transform.position - transform.position;
+                pushDirection.z = 0;
+                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * _cD.KnockBackRate);
             }
         }
     }
 
     [PunRPC]
-    void KnockBack(int targetViewID, Vector3 direction)
+    void KnockBack(int viewID, Vector3 force)
     {
-        PhotonView targetView = PhotonView.Find(targetViewID);
-        if (targetView != null)
+        PhotonView pv = PhotonView.Find(viewID);
+        if (pv != null)
         {
-            Rigidbody2D rb = targetView.GetComponent<Rigidbody2D>();
-            if (rb != null)
-            {
-                targetView.gameObject.transform.position += direction;
-                PhotonNetwork.Destroy(gameObject);
-            }
+            pv.gameObject.transform.position += force;
+            PhotonNetwork.Destroy(gameObject);
         }
+        
     }
 }
