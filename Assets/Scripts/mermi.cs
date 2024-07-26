db05f5ea776bbe8370ddd606c7e51ae0b2ba9281
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 16:10:20 +0300

    13
    13

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -3,30 +3,39 @@ using UnityEngine;
 
 public class mermi : MonoBehaviourPun
 {
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _direction; // Merminin hareket yönü
     public CharacterData _cD;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _direction = _rigidbody2D.velocity.normalized; // Baþlangýçta merminin yönünü belirleyin
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             PhotonView pv = collision.gameObject.GetComponent<PhotonView>();
-            if (pv != null && !pv.IsMine)
+            if (pv != null && !pv.IsMine) // Çarpýlan oyuncu yerel deðilse
             {
-                Vector2 pushDirection = collision.transform.position - transform.position;
-                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * 10);
+                // Merminin hareket yönünü gönder
+                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, _direction);
             }
         }
     }
 
     [PunRPC]
-    void KnockBack(int viewID, Vector2 force)
+    void KnockBack(int targetViewID, Vector3 direction)
     {
-        PhotonView pv = PhotonView.Find(viewID);
-        if (pv != null)
+        PhotonView targetView = PhotonView.Find(targetViewID);
+        if (targetView != null)
         {
-            Rigidbody2D rb = pv.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = targetView.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.AddForce(force, ForceMode2D.Impulse);
+                targetView.gameObject.transform.position += direction * _cD.KnockBackRate;
             }
         }
     }
