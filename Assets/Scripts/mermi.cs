8f97d7ca5b0d64e7e8851a829f13630a01473d03
Enesakarr50 <enesakar205@gmail.com>
2024-07-26 14:55:10 +0300

    SA
    DSASADSA

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -3,32 +3,51 @@ using UnityEngine;
 
 public class mermi : MonoBehaviourPun
 {
+    private Rigidbody2D targetRigidbody;
     public CharacterData _cD;
+
+    private void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            PhotonView pv = player.GetComponent<PhotonView>();
+            if (!pv.IsMine) // Yerel oyuncu deðilse
+            {
+                targetRigidbody = player.GetComponent<Rigidbody2D>();
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             PhotonView pv = collision.gameObject.GetComponent<PhotonView>();
-            if (pv != null && !pv.IsMine)
+            if (pv != null && !pv.IsMine) // Çarpýlan oyuncu yerel deðilse
             {
-                Vector2 pushDirection = collision.transform.position - transform.position;
-                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * 500);
-                PhotonNetwork.Destroy(gameObject);
+                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID);
             }
         }
     }
 
     [PunRPC]
-    void KnockBack(int viewID, Vector2 force)
+    void KnockBack(int targetViewID)
     {
-        PhotonView pv = PhotonView.Find(viewID);
-        if (pv != null)
+        PhotonView targetView = PhotonView.Find(targetViewID);
+        if (targetView != null)
         {
-            Rigidbody2D rb = pv.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = targetView.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.AddForce(force, ForceMode2D.Impulse);
+                Vector2 pushDirection = rb.position - (Vector2)transform.position;
+                rb.AddForce(pushDirection.normalized * 200, ForceMode2D.Impulse);
             }
+        }
+
+        if (photonView.IsMine)
+        {
+            PhotonNetwork.Destroy(gameObject);
         }
     }
 }
