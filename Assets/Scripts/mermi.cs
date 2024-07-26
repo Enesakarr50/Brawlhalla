b8e933724a3a19ca7416776d4e10408fdc65fc13
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 17:29:47 +0300

    3
    3

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -1,11 +1,9 @@
 using Photon.Pun;
-using System.Collections;
 using UnityEngine;
 
 public class mermi : MonoBehaviourPun
 {
     public CharacterData _cD;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -13,32 +11,20 @@ public class mermi : MonoBehaviourPun
             PhotonView pv = collision.gameObject.GetComponent<PhotonView>();
             if (pv != null && !pv.IsMine)
             {
-                Vector2 pushDirection = -collision.contacts[0].normal;
-                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * 200);
-                PhotonNetwork.Destroy(gameObject);
+                Vector2 pushDirection = collision.transform.position - transform.position;
+                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * 10);
+
             }
         }
     }
 
     [PunRPC]
-    void KnockBack(int viewID, Vector2 force)
+    void KnockBack(int viewID, Vector3 force)
     {
         PhotonView pv = PhotonView.Find(viewID);
         if (pv != null)
         {
-            Rigidbody2D rb = pv.GetComponent<Rigidbody2D>();
-            if (rb != null)
-            {
-                // Kuvvet uygulamasýný FixedUpdate içerisinde yapýn
-                StartCoroutine(ApplyForceAfterDelay(rb, force, 0.1f));
-            }
+            pv.gameObject.transform.position += force;
         }
-    }
-
-    private IEnumerator ApplyForceAfterDelay(Rigidbody2D rb, Vector2 force, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        rb.velocity = Vector2.zero; // Hýzý sýfýrla
-        rb.AddForce(force, ForceMode2D.Impulse); // Kuvveti uygula
     }
 }
