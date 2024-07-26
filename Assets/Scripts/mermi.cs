127c88271291dc1483f0b698aa75f3351bcd5e76
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 17:15:48 +0300

    1
    1

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -1,12 +1,34 @@
 using Photon.Pun;
 using UnityEngine;
 
-public class Mermi : MonoBehaviourPun
+public class mermi : MonoBehaviourPun
 {
     public CharacterData _cD;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        PhotonNetwork.Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PhotonView pv = collision.gameObject.GetComponent<PhotonView>();
+            if (pv != null && !pv.IsMine)
+            {
+                Vector2 pushDirection = collision.transform.position - transform.position;
+                photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, pushDirection.normalized * 500);
+                PhotonNetwork.Destroy(gameObject);
+            }
+        }
+    }
+
+    [PunRPC]
+    void KnockBack(int viewID, Vector2 force)
+    {
+        PhotonView pv = PhotonView.Find(viewID);
+        if (pv != null)
+        {
+            Rigidbody2D rb = pv.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.AddForce(force, ForceMode2D.Impulse);
+            }
+        }
     }
 }
