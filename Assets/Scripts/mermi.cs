2917463c5b9b9ef051b8f82461c76b90dbe8d8bf
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-26 16:11:02 +0300

    13
    13

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -24,6 +24,7 @@ public class mermi : MonoBehaviourPun
                 photonView.RPC("KnockBack", RpcTarget.All, pv.ViewID, _direction);
             }
         }
+        PhotonNetwork.Destroy(gameObject);
     }
 
     [PunRPC]
