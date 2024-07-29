8a8f19441a5658732063febb6fea9b8a848e66db
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-29 15:12:33 +0300

    31
    31

--- Assets/Scripts/mermi.cs
+++ Assets/Scripts/mermi.cs
@@ -15,16 +15,13 @@ public class mermi : MonoBehaviourPun
                 if (pv != null && !pv.IsMine)
                 {
                     Vector3 pushDirection = new Vector3 (collision.transform.position.x - transform.position.x, 0,0);
-                    photonView.RPC("KnockBack", RpcTarget.AllBuffered, pv.ViewID, pushDirection.normalized * kncokBack);
-                    
+                    KnockBack(pv.ViewID, pushDirection.normalized * kncokBack);
                 }
             }
 
 
         PhotonNetwork.Destroy(gameObject);
     }
-
-    [PunRPC]
     void KnockBack(int viewID, Vector3 force)
     {
         PhotonView pv = PhotonView.Find(viewID);
