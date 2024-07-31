61424cef46de25b8ee155c7d975813c3ad16a204
emirhan3699 <113527469+emirhan3699@users.noreply.github.com>
2024-07-31 16:04:08 +0300

    6
    6

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -60,7 +60,7 @@ public class CharacterSelection : MonoBehaviourPunCallbacks
 
         // Send an RPC to update the UI for all players
         bool isMine = photonView.IsMine;
-        photonView.RPC("UpdateUIForAllPlayers", RpcTarget.All, isMine, index);
+        photonView.RPC("UpdateUIForAllPlayers", RpcTarget.AllBuffered, isMine, index);
     }
 
     [PunRPC]
@@ -123,7 +123,7 @@ public class CharacterSelection : MonoBehaviourPunCallbacks
         if (CurrentData != null)
         {
            
-            photonView.RPC("spawn",RpcTarget.OthersBuffered);
+            photonView.RPC("spawn",RpcTarget.AllViaServer);
         }
         else
         {
